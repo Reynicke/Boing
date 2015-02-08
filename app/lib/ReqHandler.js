3bf0c4e0da5ce3bab043589511dda10ae1a64dcf
Reynicke <ndrs.trst@gmail.com>
2015-02-08 20:17:43 +0100

    progress queue for images during processing

--- app/lib/ReqHandler.js
+++ app/lib/ReqHandler.js
@@ -5,9 +5,6 @@ var fs = require('fs'),
     BoingRequest = require('./BoingRequest'),
     settings = require('./../settings').settings;
 
-console.log(gmImage)
-
-
 var blacklist = ['/favicon.ico'],
     fileTypes = ['png', 'jpg', 'gif'];
 
@@ -28,7 +25,7 @@ function process(request, response) {
         return;
     }
     
-    console.info(request.match.data)
+    //console.info(request.match.data)
     
     // Find source file
     var srcFilePath = findSourceFile(request.match.data['imgId']); 
@@ -37,27 +34,30 @@ function process(request, response) {
         return;
     }
 
-    prepareCacheDir(request.originalRequest.url);
+    // If we're already working on this url
+    if (!progressQueue.add(request.originalRequest.url)) {
+        sendWait(response, request.originalRequest.url);
+        return;
+    }
     
     // Manipulate and save source file
     var requestData = request.match.data; 
     var targetFile = settings.cacheDir + request.originalRequest.url;
-    
-    console.log(requestData)
+    prepareCacheDir(request.originalRequest.url);
     
     gmImage.apply(srcFilePath, requestData)
         .write(targetFile, function(err) {
             if (err) {
-                send500();                return console.dir(arguments);
+                send500(response);                
+                return console.dir(arguments);
             }
                        
             console.log(targetFile + " created  ::  " + arguments[3]);
-    
+            progressQueue.remove(request.originalRequest.url);
+            console.dir(progressQueue._queue)
+            
             // Redirect to load original url
-            response.writeHead(302, {
-                'Location': request.originalRequest.url
-            });
-            response.end();
+            sendRedirect(response, request.originalRequest.url);
     });
 }
 
@@ -91,10 +91,42 @@ function send500(response, msg) {
     response.end(msg + "\n");
 }
 
+function sendRedirect(response, url) {
+    console.log('redirect', url)
+    response.writeHead(302, {
+        'Location': url
+    });
+    response.end();
+}
+
+function sendWait(response, url) {
+    setTimeout(function() {
+        sendRedirect(response, url);
+    }, 2000);
+}
+
 function prepareCacheDir(dir) {
     var targetDir = settings.cacheDir + path.parse(dir).dir;
     mkdirp.sync(targetDir);
 }
+
+var progressQueue = {
+    _queue: [],
+    
+    add: function(url) {
+        if (this._queue.indexOf(url) === -1) {
+            this._queue.push(url);
+            return true;
+        }
+        return false;
+    },
+    
+    remove: function(url) {
+        var index = this._queue.indexOf(url);
+        this._queue.splice(index, 1);
+    }
+    
+};
 
 exports.process = process;
 
